4b3eb5cbfa14be5c5d3764b783b75da1084ef628
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2022-11-11 15:05:24 +0200

    pylint added

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,15 +1,22 @@
+''' Moduuli trie-tietorakenteen ylläpitoon'''
 class TrieNode:
+    ''' Trienode luokkaa käytetään sanojen tallettamiseen.
+    Kirjaimet ovat solmuja, ja niiden lapset seuraavia mahdollisia
+    kirjaimia sanassa. Jos solmu on jonkin sanan viimeinen kirjain,
+    talletetaan tieto solmuun.
+    '''
     def __init__(self):
+        '''Luokan konstruktori'''
         self.word = None
         self.children = {}
 
     def insert( self, word ):
+        '''Funktio sanan lisäämiseen sanastoon. Lisää sanan tietorakenteeseen
+        kirjain kerrallaan ja luo tarvittaessa uusia solmuja'''
         node = self
         for letter in word:
-            if letter not in node.children: 
+            if letter not in node.children:
                 node.children[letter] = TrieNode()
-
             node = node.children[letter]
 
         node.word = word
-
--- src/levenshtein.py
+++ src/levenshtein.py
@@ -1,57 +1,67 @@
+'''Moduuli Levenshtein -luokan ylläpitoon'''
 class Levenshtein:
+    ''' Luokan avulla lasketaan Levenshteinin etäisyys syötteenä
+    saadun sanan sekä trie-tietorakeenteen sanojen välille
+    '''
     def __init__(self, dictionary):
+        ''' Luokan konstruktori joka saa parametrikseen käytetyn sanaston'''
         self.dictionary = dictionary
 
     def execute(self):
+        ''' Pääfunktio, joka saa käyttäjältä syötteen,
+        määrittää lähimmät vaihtoehdot ja tulostaa ne käyttäjälle.'''
         word = input('Sana: \n')
         results = self.search(word)
-        results.sort(key=lambda x: x[1])
-        print(f'Sanastossa kolme lähintä sanaa:')
-        for x in range(3):
-            print(f'{results[x][0]}: Levenshtein etäisyys { results[x][1]}')
+        results.sort(key=lambda tuple: tuple[1])
+        print('Sanastossa kolme lähintä sanaa:')
+        for num in range(3):
+            print(f'{results[num][0]}: Levenshtein etäisyys { results[num][1]}')
 
-    # hakufunktio palauttaa listan pareja, jotka sisältävät jokaisen sanaston sanan sekä tämän Levenshtein-etäisyyden haettuun sanaan
     def search(self, word ):
+        '''Hakufunktio palauttaa listan pareja, jotka sisältävät jokaisen
+        sanaston sanan sekä tämän Levenshtein-etäisyyden haettuun sanaan'''
+
         # alustaa Levenshteinin matriisin
-        currentRow = range( len(word) + 1 )
+        current_row = range( len(word) + 1 )
 
         results = []
 
         # laskee rekursiivisesti etäisyyden jokaiselle sanaston sanalle
         for letter in self.dictionary.children:
-            self.searchRecursive( self.dictionary.children[letter], letter, word, currentRow, results )
-        
+            self.search_recursive( self.dictionary.children[letter], letter, word, current_row,
+            results )
+
         # tulokset järjestetään etäisyyden mukaan
         results.sort(key=lambda x: x[1])
         return results
 
-    # rekursiivinen apufunktio haun toteutukselle
-    def searchRecursive(self, node, letter, word, previousRow, results ):
-
+    def search_recursive(self, node, letter, word, previous_row, results ):
+        '''Rekursiivinen apufunktio haun toteutukselle'''
         columns = len( word ) + 1
-        # Levenshtein etäisyys ensimmäisessä kolumnissa on aina yhtä kuin rivien määrä (eli kyseisellä hetkellä tarkasteltavan osamerkkijonon pituus)
-        currentRow = [ previousRow[0] + 1 ]
+        # Levenshtein etäisyys ensimmäisessä kolumnissa on aina yhtä kuin
+        # rivien määrä (eli kyseisellä hetkellä tarkasteltavan osamerkkijonon pituus)
+        current_row = [ previous_row[0] + 1 ]
 
-        # muodostaa uuden rivin käsiteltävän kirjaimen mukaan. sarakkeiden arvot vastaavat tarkasteltavan ja tavoitesanan osamerkkijonojen Levenshtein etäisyyttä
+        # muodostaa uuden rivin käsiteltävän kirjaimen mukaan. sarakkeiden arvot
+        # vastaavat tarkasteltavan ja tavoitesanan osamerkkijonojen Levenshtein etäisyyttä
         for column in range( 1, columns ):
 
-            insertCost = currentRow[column - 1] + 1
-            deleteCost = previousRow[column] + 1
+            insert_cost = current_row[column - 1] + 1
+            delete_cost = previous_row[column] + 1
 
             if word[column - 1] != letter:
-                replaceCost = previousRow[ column - 1 ] + 1
-            else:                
-                replaceCost = previousRow[ column - 1 ]
+                replace_cost = previous_row[ column - 1 ] + 1
+            else:
+                replace_cost = previous_row[ column - 1 ]
 
-            currentRow.append( min( insertCost, deleteCost, replaceCost ) )
+            current_row.append( min( insert_cost, delete_cost, replace_cost ) )
 
-        # jos kyseinen solmu on sana, se talletetaan 
-        if node.word != None:
-            results.append( (node.word, currentRow[-1] ) )
+        # jos kyseinen solmu on sana, se talletetaan
+        if node.word is not None:
+            results.append( (node.word, current_row[-1] ) )
 
-        # rekursiivista hakua jatketaan solmun lapsille, jotta saadaan kaikkien sanojen etäisyys laskettua
-        for letter in node.children:
-            self.searchRecursive( node.children[letter], letter, word, currentRow, 
+        # rekursiivista hakua jatketaan solmun lapsille, jotta saadaan kaikkien
+        # sanojen etäisyys laskettua
+        for next_letter in node.children:
+            self.search_recursive( node.children[next_letter], next_letter, word, current_row,
                 results )
-
-

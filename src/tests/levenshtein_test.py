177a710e9daccd9f785ce27a78e27094c761cf96
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2022-12-20 16:58:38 +0200

    lisätty pari testiä

--- src/tests/levenshtein_test.py
+++ src/tests/levenshtein_test.py
@@ -15,16 +15,27 @@ class StubIO:
     
 class TestLevenshtein(unittest.TestCase):
     def setUp(self):
-        self.test_word = 'humle'
         self.dictionary = Dictionary()
         self.io = StubIO([self.test_word])
         self.calculator = Levenshtein(self.dictionary)
+    
+    def test_search_returns_correct_word_with_one_added_letter(self):
+        self.assertEqual(self.calculator.search('softwaree',1)[0], 'software')
+
+    def test_search_returns_correct_word_with_one_removed_letter(self):
+        self.assertEqual(self.calculator.search('softwre',1)[0], 'software')
+
+    def test_search_returns_correct_word_with_one_switched_letter(self):
+        self.assertEqual(self.calculator.search('softwere',1)[0], 'software')
+
+    def test_search_returns_correct_word_with_two_letters_switched(self):
+        self.assertEqual(self.calculator.search('softwrae',1)[0], 'software')
 
     def test_search_returns_correct_words_one_edit(self):
-        results = self.calculator.search(self.test_word, 1)
+        results = self.calculator.search('humle', 1)
         self.assertEqual(len(results), 2)
         self.assertListEqual(results, ['humble', 'hume'])
 
     def test_search_returns_correct_words_two_edits(self):
-        results = self.calculator.search(self.test_word, 2)
+        results = self.calculator.search('humle', 2)
         self.assertEqual(len(results), 70)
